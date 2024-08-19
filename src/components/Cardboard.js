e9ce49ecb6b4c3f895f3fe144b3037c87077a9c5
Basemism <95645899+Basemism@users.noreply.github.com>
2024-08-19 10:53:32 +0300

    Format cardboard

--- src/components/Cardboard.js
+++ src/components/Cardboard.js
@@ -5,20 +5,22 @@ const Cardboard = ({ guesses, prompt }) => {
   const found = guesses.findIndex(guess => guess.className === prompt);
 
   return (
-    <div className="cardboard">
-      <h2>Top 5 Guesses:</h2>
-      <ul>
-        {guesses.map((guess, index) => (
-          <li key={index}>
-            {index + 1}. {guess.className} - {Math.round(guess.probability * 100)}%
-          </li>
-        ))}
-      </ul>
-      <h3>
-        {found !== -1 
-          ? `Model detected in ${found + 1} guess${ found === 0 ? '!' : 'es!'}` 
-          : `Model didn't understand your drawing`}
-      </h3>
+    <div className="cardboard-container">
+      <div className="cardboard">
+        <h2>Top 5 Guesses:</h2>
+        <ol>
+          {guesses.map((guess, index) => (
+            <li key={index}>
+              {guess.className} - {Math.round(guess.probability * 100)}%
+            </li>
+          ))}
+        </ol>
+        <h3>
+          {found !== -1 
+            ? `Model detected in ${found + 1} guess${found === 0 ? '!' : 'es!'}` 
+            : `Model didn't understand your drawing`}
+        </h3>
+      </div>
     </div>
   );
 };
