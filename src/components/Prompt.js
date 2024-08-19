d274be13f19c5e673496ab3d7a148994c2dd0a8d
Basemism <95645899+Basemism@users.noreply.github.com>
2024-08-19 09:40:58 +0300

    transfered backend, fixed article issue

--- src/components/Prompt.js
+++ src/components/Prompt.js
@@ -3,12 +3,23 @@ import './Prompt.css';
 import { FaShuffle } from 'react-icons/fa6'; 
 
 const Prompt = ({ text, onShuffle }) => {
-  const vowelRegex = '^[aieouAIEOU].*';
+  const vowelRegex = '^[aieouAIEOU].*'; // Does text start with a vowel?
+  const theRegex = '^the\\s+'; // Does text start with 'the'?
+
+  const getArticle = (word) => {
+    if (word.match(theRegex)) {
+      return ''; 
+    } else if (word.match(vowelRegex)) {
+      return 'an';
+    } else {
+      return 'a';
+    }
+  };
 
   return (
     <div className="prompt-container">
       <h3 className='prompt'>
-        Draw {text.match(vowelRegex) ? 'an' : 'a'} {text}!
+        Draw {getArticle(text)} {text}!
       </h3>
       <button className="shuffle-button" onClick={onShuffle} aria-label="Shuffle Prompt">
         <FaShuffle />
